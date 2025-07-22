e8f26ff2dd9f349bb170d87c2764a32f73651eac
Agas <agasp435@gmail.com>
2025-07-22 09:52:57 +0700

    first commit

---
+++ src/main/java/com/example/turboo_ads_page/TurbooAdsPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.turboo_ads_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbooAdsPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbooAdsPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/turboo_ads_page/TurbooAdsPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turboo_ads_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbooAdsPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
