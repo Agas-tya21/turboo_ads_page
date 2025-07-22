992693d7afeb39f7053d33c1e0bde66f2ccaa88b
Agas <agasp435@gmail.com>
2025-07-22 20:33:42 +0700

    delete

--- src/main/java/com/example/turboo_ads_page/config/OpenApiConfig.java
+++ src/main/java/com/example/turboo_ads_page/config/OpenApiConfig.java
@@ -1,15 +1,20 @@
 package com.example.turboo_ads_page.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -20,18 +25,19 @@ public class OpenApiConfig {
                         .description("Dokumentasi API untuk aplikasi Turboo Ads Page. API ini menangani semua operasi terkait data promo, produk, konsumen, dan lainnya."));
     }
 
-    // Tambahkan Bean ini untuk konfigurasi CORS
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Terapkan ke semua endpoint di bawah /api
-                        .allowedOrigins("http://localhost:3000", "http://192.168.215.192:3000", "http://192.168.215.192:9090") // Izinkan origin frontend Anda
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Izinkan metode HTTP yang diperlukan
-                        .allowedHeaders("*") // Izinkan semua header
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Melayani file dari direktori "uploads" melalui URL /uploads/**
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadDir + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Terapkan ke semua endpoint di bawah /api
+                .allowedOrigins("http://localhost:3000", "http://192.168.215.192:3000", "http://192.168.215.192:9090") // Izinkan origin frontend Anda
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Izinkan metode HTTP yang diperlukan
+                .allowedHeaders("*") // Izinkan semua header
+                .allowCredentials(true);
     }
 }
--- src/main/java/com/example/turboo_ads_page/repository/KonsumenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.turboo_ads_page.repository;
-
-import com.example.turboo_ads_page.entity.Konsumen;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KonsumenRepository extends JpaRepository<Konsumen, String> {
-}
