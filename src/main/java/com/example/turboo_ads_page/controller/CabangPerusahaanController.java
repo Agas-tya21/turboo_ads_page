47ba3e28300b75b613d4de15fda9faf033a46888
Agas <agasp435@gmail.com>
2025-07-23 10:31:43 +0700

    -

--- src/main/java/com/example/turboo_ads_page/controller/CabangPerusahaanController.java
+++ src/main/java/com/example/turboo_ads_page/controller/CabangPerusahaanController.java
@@ -1,15 +1,24 @@
 package com.example.turboo_ads_page.controller;
 
-import com.example.turboo_ads_page.model.CabangPerusahaan;
-import com.example.turboo_ads_page.service.CabangPerusahaanService;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import com.example.turboo_ads_page.model.CabangPerusahaan;
+import com.example.turboo_ads_page.service.CabangPerusahaanService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/cabang")
